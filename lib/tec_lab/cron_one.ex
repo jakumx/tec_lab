6964b0ab73c3c35a9544ee5c2cbcb2face232ca4
josuemx <artu_ja@hotmail.com>
2016-10-28 10:27:08 -0500

    after -> creando csv

--- lib/tec_lab/cron_one.ex
+++ lib/tec_lab/cron_one.ex
@@ -13,18 +13,14 @@ defmodule TecLab.CronOne do
   def handle_info(:work, state) do
 
     files_found = Redis.all()
-    #FIXME: add to list_File 
-    list_File = []
-    for n <- files_found do
+    list_File = for n <- files_found do
       IO.inspect n
       file_found = Redis.get(n)
         |> Crypt.de
         |> String.split(",")
-      new_file = List.insert_at([], -1, file_found)
-        |> IO.inspect
-      list_File ++ new_file
-      IO.inspect list_File
+      file_found
     end
+    csv_content(list_File)
     schedule_work()
     {:noreply, state}
   end
